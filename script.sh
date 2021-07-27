620efd257bc3735705e8e067c400eca37bcb68a4
navet <83888350+GTX1O8OTi@users.noreply.github.com>
2021-07-27 20:25:07 +0000

    Update script.sh

--- script.sh
+++ script.sh
@@ -3,11 +3,11 @@ mount /dev/sda2 /mnt
 mkdir -p /mnt/boot/efi
 mount /dev/sda1 /mnt/boot/efi
 
-pacstrap /mnt base linux linux-firmware
+pacstrap /mnt base linux linux-firmware sudo nvidia plasma nano firefox konsole kde networkmanager dhcpcd sddm git wget vim grub efibootmgr
+
+genfstab -U /mnt >> /mnt/etc/fstab
 
 arch-chroot /mnt
-
-pacman -S sudo nvidia plasma nano firefox konsole kde networkmanager dhcpcd sddm git wget
 
 systemctl enable NetworkManager
 systemctl enable dhcpcd
@@ -18,6 +18,11 @@ echo 'export EDITOR=nano'
 echo 'visudo'
 echo 'mude a linha la do ALL (ALL) alguma coisa'
 
+mkinitcpio -P
+
 echo -e "2112\n2112" | passwd
 useradd -m tevin
 echo -e "2112\n2112" | passwd tevin
+
+grub-install
+grub-mkconfig -o /boot/grub/grub.cfg
